c9c22c434faa81594d4a763202659ae1b9a9a3b3
Matthew Osborn <matthew@osbornm.com>
2012-10-17 19:37:55 -0700

    Starting to port to OWIN. Sadly OWIN is not perfect yet so it will be awhile before I can do this...

---
+++ Playr.Api/Startup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Owin;
+using Gate.Middleware;
+using SignalR;
+using SignalR.Hosting.Owin;
+
+namespace Playr.Api
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app
+                .UseShowExceptions();
+                
+        }
+    }
+}
